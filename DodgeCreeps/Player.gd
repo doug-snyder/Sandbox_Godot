42639340c0b4c19ab7a2b85f8c3556673c7b3e6e
Doug Snyder <mistershake2099@gmail.com>
2018-03-28 00:46:15 -0700

    Fleshing out a super basic game in Godot's Scene-Node style.

--- DodgeCreeps/Player.gd
+++ DodgeCreeps/Player.gd
@@ -1,12 +1,18 @@
 extends Area2D
 
 
+signal hit
+
+
 export (int) var SPEED
 var velocity = Vector2()
 var screensize
 
+
 func _ready():
 	screensize = get_viewport_rect().size
+	hide()
+	
 	
 func _process(delta):
 	velocity = Vector2()
@@ -24,7 +30,27 @@ func _process(delta):
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
-
+		
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+	
 	position += velocity * delta
 	position.x = clamp( position.x, 0, screensize.x )
 	position.y = clamp( position.y, 0, screensize.y )
+
+
+func start( pos ):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
+
+func _on_Player_body_entered( body ):
+	hide()
+	emit_signal( hit )
+	$CollisionShape2D.disabled = true
