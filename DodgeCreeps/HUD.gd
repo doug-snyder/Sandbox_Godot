a8bf7d19ead465a535464887e28e54dd69ca106c
MisterShake2099 <mistershake2099@gmail.com>
2018-03-29 15:38:21 -0700

    Dodge the Creeps works in its own simple way.

--- DodgeCreeps/HUD.gd
+++ DodgeCreeps/HUD.gd
@@ -19,7 +19,7 @@ func show_game_over():
 
 
 func update_score(score):
-	$ScoreLabel.text = str(score)
+	$ScoreLabel.text = str( score )
 
 
 func _on_StartButton_pressed():
--- DodgeCreeps/Main.gd
+++ DodgeCreeps/Main.gd
@@ -11,10 +11,10 @@ func _ready():
 
 func new_game():
 	score = 0
-	$HUD.update_score(score)
-	$Player.start($StartPosition.position)
+	$HUD.update_score( score )
+	$Player.start( $StartPosition.position )
 	$StartTimer.start()
-	$HUD.show_message("Get Ready")
+	$HUD.show_message( "Get Ready" )
 	$Music.play()
 
 
@@ -28,15 +28,15 @@ func game_over():
 
 func _on_MobTimer_timeout():
 	# choose a random location on the Path2D
-	$MobPath/MobSpawnLocation.set_offset(randi())
+	$MobPath/MobSpawnLocation.set_offset( randi() )
 	var mob = Mob.instance()
-	add_child(mob)
+	add_child( mob )
 	var direction = $MobPath/MobSpawnLocation.rotation + PI/2
 	mob.position = $MobPath/MobSpawnLocation.position
 	# add some randomness to the direction
-	direction += rand_range(-PI/4, PI/4)
+	direction += rand_range( -PI/4, PI/4 )
 	mob.rotation = direction
-	mob.set_linear_velocity(Vector2(rand_range(mob.MIN_SPEED, mob.MAX_SPEED), 0).rotated(direction))
+	mob.set_linear_velocity( Vector2(rand_range(mob.MIN_SPEED, mob.MAX_SPEED), 0).rotated(direction) )
 
 
 func _on_StartTimer_timeout():
