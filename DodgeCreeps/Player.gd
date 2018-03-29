cd76f522b2c96578690815f7adaed59862a0e60a
Doug Snyder <mistershake2099@gmail.com>
2018-03-29 01:05:20 -0700

    more or less completed, but won't start for some reason

--- DodgeCreeps/Player.gd
+++ DodgeCreeps/Player.gd
@@ -1,36 +1,42 @@
 extends Area2D
 
-
 signal hit
-
 
 export (int) var SPEED
 var velocity = Vector2()
 var screensize
 
-
 func _ready():
-	screensize = get_viewport_rect().size
 	hide()
-	
-	
+	screensize = get_viewport_rect().size
+
+func start(pos):
+	position = pos
+	show()
+	$Collision.disabled = false
+
 func _process(delta):
 	velocity = Vector2()
-	
-	if Input.is_action_pressed( "ui_right" ):
+	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-	if Input.is_action_pressed( "ui_left" ):
+	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed( "ui_down" ):
+	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
-	if Input.is_action_pressed( "ui_up" ):
+	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
 		$AnimatedSprite.play()
+		#$Trail.emitting = true
 	else:
 		$AnimatedSprite.stop()
-		
+		#$Trail.emitting = false
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "right"
 		$AnimatedSprite.flip_v = false
@@ -38,19 +44,11 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
-	
-	position += velocity * delta
-	position.x = clamp( position.x, 0, screensize.x )
-	position.y = clamp( position.y, 0, screensize.y )
-
-
-func start( pos ):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
-	
 
 func _on_Player_body_entered( body ):
+	$Collision.disabled = true
 	hide()
-	emit_signal( hit )
-	$CollisionShape2D.disabled = true
+	emit_signal("hit")
+
+
+
